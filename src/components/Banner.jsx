d09da5de1a758c856685ca425c4a1a7b4c27acd7
FuadTalukder85 <webfuadtk@gmail.com>
2025-03-03 00:21:49 +0600

    added navbar

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Banner = () => {
   return (
     <div>
-      <h1>This is banner</h1>
+      <h1 className="text-3xl text-red-400">Work Experience</h1>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import logo from "../assets/logo.png";
+const menu = ["Home", "Products", "Support", "About", "Contact"];
 const Navbar = () => {
-  return <div>This is navbar</div>;
+  return (
+    <div className="bg-[#1A0B2E] py-10">
+      <div className="max-w-5xl mx-auto flex justify-between">
+        <img src={logo}></img>
+        <ul className="flex gap-12 items-center font-semibold text-xl">
+          {menu?.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
