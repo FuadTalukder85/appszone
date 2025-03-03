21c4a1bd46534ae358fa9459fb7ebe824297cb28
FuadTalukder85 <webfuadtk@gmail.com>
2025-03-04 01:13:23 +0600

    added contact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import icon01 from "../assets/icon01.png";
+import icon02 from "../assets/icon02.png";
+import icon03 from "../assets/icon03.png";
+
+const Contact = () => {
+  return (
+    <div className="max-w-5xl mx-auto py-16">
+      <h1 className="text-2xl">Contact</h1>
+      <h5 className="text-[15px] mt-16 w-3xl">
+        I'm currently looking to join a cross-functional team that values
+        improving people's lives through accessible design. or have a project in
+        mind? Let's connect.
+      </h5>
+      <h5 className="text-[15px] mt-7">ibrhaimmemon930@gmail.com</h5>
+      <div className="flex gap-4 items-center mt-7">
+        <img src={icon01} alt="" /> <img src={icon02} alt="" />{" "}
+        <img src={icon03} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
+<h1>Contact</h1>;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Contact from "../components/Contact";
 
 const MainLayout = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Contact></Contact>
     </div>
   );
 };
