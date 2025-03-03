2b279ad19f5b01359526581e93ae4d948be55ca4
Fuad Talukder <webfuadtk@gmail.com>
2025-03-03 15:44:38 +0600

    add experience

---
+++ src/components/Experience.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import img01 from "../assets/img01.png";
+import img02 from "../assets/img02.png";
+import img03 from "../assets/img03.png";
+import img04 from "../assets/img04.png";
+const data = [
+  {
+    title: "CIB on the Mobile",
+    description:
+      "Take your client onboard seamlessly by our amazing tool of digital onboard process.",
+    btn: "LEARN MORE",
+    img: img01,
+  },
+  {
+    title: "CIB on the Mobile",
+    description:
+      "Take your client onboard seamlessly by our amazing tool of digital onboard process.",
+    btn: "LEARN MORE",
+    img: img02,
+  },
+  {
+    title: "CIB on the Mobile",
+    description:
+      "Take your client onboard seamlessly by our amazing tool of digital onboard process.",
+    btn: "LEARN MORE",
+    img: img03,
+  },
+  {
+    title: "CIB on the Mobile",
+    description:
+      "Take your client onboard seamlessly by our amazing tool of digital onboard process.",
+    btn: "LEARN MORE",
+    img: img04,
+  },
+];
+const Experience = () => {
+  return (
+    <div
+      className="max-w-5xl mx-auto pb-20"
+      style={{
+        background:
+          "radial-gradient(circle at center, rgba(105, 59, 147, 1) 0%,  rgba(50, 15, 133, 0) 40%, rgb(0, 0, 0,0) 40%)",
+        borderRadius: "100%",
+      }}
+    >
+      <h1 className="text-[40px]">Work Experience</h1>
+      <div className="grid grid-cols-2 gap-5 mt-10">
+        {data?.map((dt) => (
+          <div className="flex gap-4 items-center ps-5 pr-28 py-5 border-t-4 border-[#4F228D] rounded-2xl bg-[linear-gradient(to_right,#130428,#251043,#38126D,#261045,#190634)]">
+            <img src={dt.img} alt="" />
+            <div>
+              <h2 className="font-semibold text-2xl">{dt.title}</h2>
+              <p className="font-medium text-[8px]">{dt.description}</p>
+              <button className="mt-2 px-6 py-2 rounded-lg border border-[#693B93] bg-[#2C1250] font-medium text-[10px] uppercase hover:bg-[#693B93] transition-all duration-300 cursor-pointer">
+                {dt.btn}
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import About from "../components/About-me";
+import Experience from "../components/Experience";
 
 const Home = () => {
   return (
     <div>
       <About></About>
+      <Experience></Experience>
     </div>
   );
 };
